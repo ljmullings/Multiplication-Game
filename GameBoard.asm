bc27eb5b1e0c1bacf4686f69c5e941cb2bf0cc69
Laura Chan-Mullings <40129588+ljmullings@users.noreply.github.com>
2023-11-28 23:06:50 -0600

    Update GameBoard.asm

--- GameBoard.asm
+++ GameBoard.asm
@@ -1,77 +1,72 @@
-
-# Data Segment
 .data
-    .globl asciiTable
-    .globl rowSize
-    .globl numRows
-    .globl additionalLine
-    .globl additionalLineSize
-    .globl prompt
-    .globl upperBound
-    .globl seed
-    .globl currentTurn
-    #.globl selectionList
-
-
-seed: 
+	.globl prompt
+	.globl seed
+	seed: 
 	.word 123456789  # Initial seed value
-asciiTable: 
-    .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 49, 54, 56, 63, 64, 72, 81
+	prompt: 
+    		.asciiz "Enter your number (1-9): "
+    	BoardRows: .word 6
+    	BoardCols: .word 6
+	bitmapData: .space 36  # 36 bytes for a 6x6 grid
+	labelMap:   .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 49, 54, 56, 63, 64, 72, 81	# Array mapping each cell to its label
 
-#selectionList:
-    #.byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
 
-rowSize: 
-    .word 6
-
-numRows: 
-    .word 6
-
-additionalLine: 
-    .word 1, 2, 3, 4, 5, 6, 7, 8, 9
-
-additionalLineSize: 
-    .word 9
-
-prompt: 
-    .asciiz "Enter your number (1-9): "
-
-upperBound: 
-    .word 9 # Upper bound for random number generation
-
-# Text Segment
 .text
 .globl displayGameboard
 displayGameboard:
-    
-    # Load table base addresses
-    la $t0, asciiTable 
-    #la $t4, selectionList #Assume $t2, $t3 are used for loop counters or other purpose
+    # Save the original value of $s0
+    addi $sp, $sp, -4
+    sw $s0, 0($sp)
 
-    # Load row and column sizes
-    lw $t1, rowSize
-    lw $t2, numRows
+    la $t0, bitmapData   # Load address of bitmap
+    la $t1, labelMap     # Load address of label map
 
+    li $t2, 6            # Number of rows
+    li $t3, 0            # Row counter
 
-    # Row loop
     row_loop:
-        beq $t2, $zero, display_additional_line # Go to additional line display if all rows are done
-        move $t3, $t1 # Initializes the column counter $t3 with the size of a row $t1
-        
-        # Column loop
-        col_loop:
-            beq $t3, $zero, next_row # checks if all columns in the current row have been processed. If so, it jups to next row
-            lw $a0, 0($t0) # Load the current table element into $a0
-            li $v0, 1 # Prepare to print integer
-            syscall # printing the integer in $a0
+        beq $t3, $t2, end_display   # Check if all rows are processed
+        li $t4, 6                    # Number of columns
+        li $t5, 0                    # Column counter
 
-            # Print a space
-            li $a0, 32 
-            li $v0, 11
+        col_loop:
+            beq $t5, $t4, next_row   # Check if all columns in this row are processed
+
+            # Calculate index in bitmapData (1D index from 2D coordinates)
+            mul $t6, $t3, $t4        # row_index * row_size
+            add $t6, $t6, $t5        # + col_index
+            add $s0, $t0, $t6        # Address of the current cell in bitmapData
+
+            # Load the byte value from bitmapData
+            lb $t7, 0($s0)
+
+            # Check if the cell contains 'X' (88) or 'O' (79)
+            li $t8, 88               # ASCII for 'X'
+            beq $t7, $t8, print_char
+            li $t8, 79               # ASCII for 'O'
+            beq $t7, $t8, print_char
+
+            # For numbers, load from labelMap
+            sll $t8, $t6, 2          # Convert index to word offset
+            add $t8, $t1, $t8        # Address of the number in labelMap
+            lw $a0, 0($t8)           # Load the number to print
+            li $v0, 1                # Syscall for printing integer
             syscall
 
-            addiu $t0, $t0, 4 # Move to the next table element
-            addiu $t3, $t3, -1 # Decrement column counter
+            j print_space
+
+            print_char:
+            move $a0, $t7            # Load 'X' or 'O'
+            li $v0, 11               # Syscall for printing character
+            syscall
+
+            print_space:
+            # Optionally print a separator like a space
+            li $a0, 32               # ASCII code for space
+            li $v0, 11               # Syscall for printing character
+            syscall
+
+            addi $t5, $t5, 1         # Increment column counter
             j col_loop
 
         next_row:
@@ -80,32 +75,15 @@ displayGameboard:
             li $v0, 11
             syscall
 
-            addiu $t2, $t2, -1 # Decrement row counter
-            j row_loop # jumps back to the start of the row loop to process the next row
+            addi $t3, $t3, 1         # Increment row counter
+            j row_loop
 
-    display_additional_line:
-        # Load additional line base address and size
-        la $t0, additionalLine
-        lw $t1, additionalLineSize
-        move $t3, $t1 # Element counter for additional line
-
-        # Loop to display additional line
-        additional_line_loop:
-            beq $t3, $zero, end_display # Go to game loop if all elements are displayed
-            lw $a0, 0($t0) # Load the current element
-            li $v0, 1 # Prepare to print integer
-            syscall
-
-            # Print a space
-            li $a0, 32
-            li $v0, 11
-            syscall
-
-            addiu $t0, $t0, 4 # Move to the next element
-            addiu $t3, $t3, -1 # Decrement element counter
-            j additional_line_loop
-     jr $ra # Return to caller 
-     
+    end_display:
+    # Restore the original value of $s0 and return
+    lw $s0, 0($sp)
+    addi $sp, $sp, 4
+    jr $ra
+            
 # Procedure for Random Number Generation
 .globl randomGenerator
 randomGenerator:
@@ -132,6 +110,59 @@ randomGenerator:
     sw $v0, seed
 
     jr $ra           # Return the random number in $v0
+    
+# Procedure to alter the game board based on the current turn and selection
+# Parameters:
+# $a0 - Turn indicator (0 for user, 1 for computer)
+# $a1 - Selection (number chosen)
+.globl alter_board
+alter_board:
+    # Save registers
+    addi $sp, $sp, -8
+    sw $t0, 0($sp)
+    sw $t1, 4($sp)
 
-end_display:
-    jr $ra # Return to the caller
+    # Initialize loop variables
+    li $t0, 0          # Index for looping through labelMap
+    la $t1, labelMap   # Address of labelMap
+
+    find_index_loop:
+        # Check if we've reached the end of labelMap (36 elements)
+        li $t2, 36
+        beq $t0, $t2, index_not_found
+
+        # Load the current label
+        lw $t2, 0($t1)
+        addi $t1, $t1, 4  # Move to the next label
+
+        # Compare with the product
+        beq $t2, $a1, index_found
+
+        addi $t0, $t0, 1  # Increment index
+        j find_index_loop
+
+    index_not_found:
+        # Product not found in labelMap, handle error
+        # ... error handling code ...
+
+    index_found:
+        # $t0 now contains the correct index
+        la $t1, bitmapData
+        add $t1, $t1, $t0  # Address of the correct cell in bitmapData
+
+        # Store ASCII 'X' for user (88) and 'O' for computer (79)
+        beq $a0, $zero, mark_user
+        li $t2, 79        # ASCII for 'O'
+        j update_board
+
+    mark_user:
+        li $t2, 88        # ASCII for 'X'
+
+    update_board:
+        sb $t2, 0($t1)    # Store the value in the selected cell
+
+    # Restore registers and return
+    lw $t0, 0($sp)
+    lw $t1, 4($sp)
+    addi $sp, $sp, 8
+    jr $ra
