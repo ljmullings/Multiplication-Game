182d1e223fcde031c459ce9141d7f01567831325
Laura Chan-Mullings <40129588+ljmullings@users.noreply.github.com>
2023-11-29 16:01:13 -0600

    Update GameBoard.asm

--- GameBoard.asm
+++ GameBoard.asm
@@ -1,18 +1,21 @@
 .data
 	.globl prompt
 	.globl seed
-	seed: 
-	.word 123456789  # Initial seed value
-	prompt: 
-    		.asciiz "Enter your number (1-9): "
-    	BoardRows: .word 6
-    	BoardCols: .word 6
-	bitmapData: .space 36  # 36 bytes for a 6x6 grid
-	labelMap:   .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 49, 54, 56, 63, 64, 72, 81	# Array mapping each cell to its label
-
+	.globl win_message
+    	.globl you_lose_prompt
+    	win_message:  	.asciiz "You win!"
+    	you_lose_prompt:.asciiz "You lose!"
+    	game_over:	.asciiz "Game Over!"
+	seed: 		.word 123456789  # Initial seed value
+	prompt: 	.asciiz "Enter your number (1-9): "
+    	BoardRows: 	.word 6
+    	BoardCols: 	.word 6
+	bitmapData: 	.space 36  # 36 bytes for a 6x6 grid
+	labelMap:   	.word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 49, 54, 56, 63, 64, 72, 81	# Array mapping each cell to its label
+	debug_statement: .asciiz "CODE VISITED"
 
 .text
-.globl displayGameboard
+.globl displayGameboard # ----------------------------------------- DISPLAY GAMEBOARD CODE: FINISHED DO NOT TOUCH
 displayGameboard:
     # Save the original value of $s0
     addi $sp, $sp, -4
@@ -84,7 +87,7 @@ displayGameboard:
     addi $sp, $sp, 4
     jr $ra
             
-# Procedure for Random Number Generation
+# Procedure for Random Number Generation  -------------------- RANDOM NUMBER GENERATOR CODE: FINISHED DO NOT TOUCH
 .globl randomGenerator
 randomGenerator:
     # Simple linear congruential generator formula: X_{n+1} = (a * X_n + c) % m
@@ -111,7 +114,7 @@ randomGenerator:
 
     jr $ra           # Return the random number in $v0
     
-# Procedure to alter the game board based on the current turn and selection
+# Procedure to alter the game board based on the current turn and selection -------------------- ALTERING THE GAMEBOARD CODE: FINISHED DO NOT TOUCH AND HAS BEEN DEBUGGED
 # Parameters:
 # $a0 - Turn indicator (0 for user, 1 for computer)
 # $a1 - Selection (number chosen)
@@ -166,3 +169,248 @@ alter_board:
     lw $t1, 4($sp)
     addi $sp, $sp, 8
     jr $ra
+# ---------------------------------------------------------------------------- CHECKING STATUS CODE: DEBUG STAGE, BE CAREFUL WITH THIS
+.globl print_debug_statement
+print_debug_statement:
+    li $v0, 4              # Syscall code for print string
+    la $a0, debug_statement # Load address of the debug statement
+    syscall                # Execute syscall
+
+    jr $ra                 # Return to the calling function
+
+.globl check_for_win
+check_for_win:
+    # Assume $a1 contains the index of the last tile placed
+    # $a0 contains the player who made the move (0 for user, 1 for computer)
+	# jal print_debug_statement Visited!
+    	# Call procedures to check for wins
+    	# Save $ra to stack if needed
+    	addi $sp, $sp, -4
+    	sw $ra, 0($sp)
+    	
+    	move $a0, $a1
+    	# jal print_debug_statement VISITED
+    	jal check_horizontal_win
+    	beq $v0, 1, win_user
+    	beq $v0, 2, win_computer
+
+    	jal check_vertical_win
+    	beq $v0, 1, win_user
+    	beq $v0, 2, win_computer
+
+    	jal check_diagonal_win
+    	beq $v0, 1, win_user
+    	beq $v0, 2, win_computer
+
+	# Restore $ra and return
+	# Restore $ra and return
+    	lw $ra, 0($sp)
+    	addi $sp, $sp, 4
+    	jr $ra
+    	jr $ra  # No win, return to the main loop
+    	
+win_user:
+    # Handle user win (print "You Win!" etc.)
+    # ...
+
+    j end_game  # End the game after the win
+
+win_computer:
+    # Handle computer win (print "You Lose!" etc.)
+    # ...
+
+    j end_game  # End the game after the win
+
+end_game:
+    # Optional: Print a message before ending the game
+     li $v0, 4               # Syscall code for print string
+     la $a0, game_over    # Address of the message to print
+     syscall                 # Execute syscall
+
+    # Terminate the program
+    li $v0, 10               # Syscall code for exit
+    syscall                  # Execute syscall
+
+    # This line is technically not reached after the syscall for exit
+    jr $ra                   # Return to the calling function
+    
+.globl check_horizontal_win
+check_horizontal_win:
+    # Save registers
+    addi $sp, $sp, -20
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)  # Address of bitmapData
+    sw $s1, 8($sp)  # Counter for 'X's
+    sw $s2, 12($sp) # Counter for 'O's
+    sw $s3, 16($sp) # Row and column index
+
+	# jal print_debug_statement VISITED
+    # Initialize base address of bitmapData
+    la $s0, bitmapData
+
+    # Iterate through each row
+    li $s3, 0
+    row_loop_horizontal:
+        li $t0, 6
+        beq $s3, $t0, end_horizontal_check  # End if all rows processed
+        # Reset counters for each new row
+        li $s1, 0
+        li $s2, 0
+
+        # Iterate through each column in the row
+        li $t1, 0
+        column_loop_horizontal:
+            beq $t1, $t0, next_row_horizontal  # End if all columns processed
+	
+            # Calculate index in bitmapData
+            mul $t2, $s3, $t0
+            add $t2, $t2, $t1
+            add $t2, $s0, $t2
+
+            # Load the value at the current cell
+            lb $t3, 0($t2)
+
+            # Check for 'X' or 'O'
+            li $t4, 88  # ASCII for 'X'
+            beq $t3, $t4, check_x_horizontal
+            li $t4, 79  # ASCII for 'O'
+            beq $t3, $t4, check_o_horizontal
+
+            # Reset counters if the current cell is neither 'X' nor 'O'
+            li $s1, 0
+            li $s2, 0
+            j continue_horizontal
+
+            check_x_horizontal:
+            addi $s1, $s1, 1  # Increment 'X' counter
+            li $s2, 0         # Reset 'O' counter
+            j check_win_horizontal
+
+            check_o_horizontal:
+            addi $s2, $s2, 1  # Increment 'O' counter
+            li $s1, 0         # Reset 'X' counter
+
+            check_win_horizontal:
+            # Check if four 'X's or 'O's in a row are found
+            li $t4, 4
+            beq $s1, $t4, win_x_horizontal
+            beq $s2, $t4, win_o_horizontal
+
+            continue_horizontal:
+            addi $t1, $t1, 1
+            j column_loop_horizontal
+
+        next_row_horizontal:
+        addi $s3, $s3, 1
+        j row_loop_horizontal
+
+    end_horizontal_check:
+    # No win detected, set $v0 to 0
+    li $v0, 0
+  
+    j restore_and_return_horizontal
+
+    win_x_horizontal:
+    # 'X' win detected, set $v0 to 2
+    li $v0, 2
+    j restore_and_return_horizontal
+
+    win_o_horizontal:
+    # 'O' win detected, set $v0 to 1
+    li $v0, 1
+
+    restore_and_return_horizontal:
+    # Restore registers and return
+    lw $ra, 0($sp)
+    lw $s0, 4($sp)
+    lw $s1, 8($sp)
+    lw $s2, 12($sp)
+    lw $s3, 16($sp)
+    addi $sp, $sp, 20
+    jr $ra
+
+	
+.globl check_vertical_win
+check_vertical_win:
+    # Save necessary registers
+    addi $sp, $sp, -12
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)  # Temporarily store bitmapData address
+    sw $s1, 8($sp)  # Temporarily store consecutive count
+
+    la $s0, bitmapData  # Load base address of bitmapData
+
+    # Iterate over each column
+    li $t0, 0  # Column index
+    column_loop1:
+   
+        li $s1, 0   # Reset consecutive count for 'X'
+        li $t1, 0   # Reset consecutive count for 'O'
+        li $t2, 0   # Row index
+
+        # Check each cell in the column
+        row_loop1:
+            # Calculate address for the current cell
+            mul $t3, $t2, 6  # Calculate row offset
+            add $t3, $t3, $t0 # Add column index
+            add $t3, $s0, $t3 # Calculate actual address
+
+            # Load the value from the current cell
+            lb $t4, 0($t3)
+
+            # Check for 'X' or 'O'
+            li $t5, 88       # ASCII for 'X'
+            li $t6, 79       # ASCII for 'O'
+            beq $t4, $t5, check_X
+            beq $t4, $t6, check_O
+            # Reset count if neither 'X' nor 'O'
+            li $s1, 0
+            li $t1, 0
+            j check_next_row
+
+            check_X:
+            addi $s1, $s1, 1  # Increment count for 'X'
+            li $t1, 0         # Reset count for 'O'
+            j check_win
+
+            check_O:
+            addi $t1, $t1, 1  # Increment count for 'O'
+            li $s1, 0         # Reset count for 'X'
+
+            check_win:
+            # Check if four in a row are found
+            li $t7, 4
+            beq $s1, $t7, user_win
+            beq $t1, $t7, computer_win
+
+            check_next_row:
+            addi $t2, $t2, 1
+            li $t8, 6
+            blt $t2, $t8, row_loop1 # Check next row if within bounds
+
+        # Move to the next column
+        addi $t0, $t0, 1
+        li $t8, 6
+        blt $t0, $t8, column_loop1 # Check next column if within bounds
+
+    # No win detected
+    li $v0, 0
+
+    j restore_and_return
+
+    user_win:
+    li $v0, 1  # Set return value to 1 for user win
+    j restore_and_return
+
+    computer_win:
+    li $v0, 2  # Set return value to 2 for computer win
+
+    restore_and_return:
+    # Restore registers
+    lw $ra, 0($sp)
+    lw $s0, 4($sp)
+    lw $s1, 8($sp)
+    addi $sp, $sp, 12
+    jr $ra
+check_diagonal_win:
+	jr $ra
