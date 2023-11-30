2bacf75d8de3d1e7284bafffdd6f19986760820c
Laura Chan-Mullings <40129588+ljmullings@users.noreply.github.com>
2023-11-29 22:34:58 -0600

    Update LogicHandler.asm

--- LogicHandler.asm
+++ LogicHandler.asm
@@ -1,12 +1,6 @@
-# LogicHandler.asm File
-
-.data
-
 .text
 .globl multiplyNumbers
-# Multiplies two numbers and returns the product
 multiplyNumbers:
-    # Assuming the numbers to be multiplied are passed in $a0 and $a1
-    mul $v0, $a0, $a1  # Multiply $a0 and $a1, store the result in $v0
-    jr $ra             # Return the product in $v0
+    mul $v0, $a0, $a1  
+    jr $ra             
 
